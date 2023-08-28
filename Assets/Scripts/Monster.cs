178b1707cf2a5e0a9e804e0cfb6ce97be0c6cb43
leejung7308 <leekkzzzz@naver.com>
2023-08-28 21:58:23 +0900

    feat : Monster Movement
    레이더 감지 : 플레이어 추격
    레이더 비감지 : 스폰 위치로 복귀

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject radarObject;
+    public float moveSpeed = 5f;
+    public Vector2 spawnPoint;
+    Radar radar;
+    GameObject player;
+    private void Start()
+    {
+        radar = radarObject.GetComponent<Radar>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        MonsterMovement();
+    }
+    private void MonsterMovement()
+    {
+        if (radar.isDetected) //플레이어가 레이더에 감지 되면 플레이어를 따라감
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Time.deltaTime * moveSpeed);
+        }
+        else //플레이어가 레이더를 벗어나면 원래 위치(스폰 위치)로 돌아감
+        {
+            transform.position = Vector2.MoveTowards(transform.position, spawnPoint, Time.deltaTime * moveSpeed);
+        }
+    }
+}
