686409611b7341504552d2f31cd4a60bcec287bc
leejung7308 <leekkzzzz@naver.com>
2023-09-26 00:42:48 +0900

    feat: Add light image masking
    18시~20시 day~night 전환 이미지 마스킹
    24시(00시) 눈 깜빡이면서 잠드는 애니메이션 구현

--- Assets/Scripts/WorldTime.cs
+++ Assets/Scripts/WorldTime.cs
@@ -8,13 +8,18 @@ public class WorldTime : MonoBehaviour
 {
     private float currentTime;
     public TMP_Text timeText;
+    public Image lightImage;
+    public Color day;
+    public Color evening;
+    public Color night;
+    private bool isSleep = false;
     void Start()
     {
         ResetWorldTime();
     }
     void Update()
     {
-        UpdateWorldTime();
+        if (!isSleep) UpdateWorldTime();
     }
     public void ResetWorldTime()
     {
@@ -26,13 +31,63 @@ public class WorldTime : MonoBehaviour
     }
     private void UpdateWorldTime()
     {
-        currentTime += Time.deltaTime;
-        float hour = GetCurrentTime() / 60f;
-        float minute = GetCurrentTime() % 60f;
+        currentTime += Time.deltaTime*60;
+        float hour = currentTime / 60f;
+        float minute = currentTime % 60f;
         timeText.text = string.Format("Time {0:D2} : {1:D2}", (int)hour, (int)minute);
-        if (hour >= 24f)
+        if ((int)hour == 24)
         {
             ResetWorldTime();
+            isSleep = true;
+            if(isSleep) StartCoroutine(Sleep());
         }
+        if (hour >= 8 && hour <= 18)
+        {
+            lightImage.color = Color.Lerp(day, evening, (hour - 8) / 10);
+        }
+        else if(hour >= 18 && hour <= 20)
+        {
+            lightImage.color = Color.Lerp(evening, night, (hour - 18) / 2);
+        }
+    }
+    public IEnumerator Sleep()
+    {
+        timeText.gameObject.SetActive(false);
+        float fade = 0;
+        Color originalColor = lightImage.color;
+        for (int i = 0; i < 2; i++)
+        {
+            fade = 0;
+            for (int j = 0; j < 100; j++)
+            {
+                fade += 0.01f;
+                lightImage.color = Color.Lerp(originalColor, new Color(0, 0, 0, 1), fade);
+                yield return new WaitForSeconds(0.005f);
+            }
+            fade = 0;
+            for (int j = 0; j < 100; j++)
+            {
+                fade += 0.01f;
+                lightImage.color = Color.Lerp(new Color(0, 0, 0, 1), originalColor, fade);
+                yield return new WaitForSeconds(0.005f);
+            }
+        }
+        fade = 0;
+        for (int j = 0; j < 100; j++)
+        {
+            fade += 0.01f;
+            lightImage.color = Color.Lerp(originalColor, new Color(0, 0, 0, 1), fade);
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(2f);
+        fade = 0;
+        for (int j = 0; j < 100; j++)
+        {
+            fade += 0.01f;
+            lightImage.color = Color.Lerp(new Color(0, 0, 0, 1), new Color(0,0,0,0), fade); ;
+            yield return new WaitForSeconds(0.01f);
+        }
+        timeText.gameObject.SetActive(true);
+        isSleep = false;
     }
 }
