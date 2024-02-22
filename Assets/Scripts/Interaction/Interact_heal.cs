e933a5f33d5b1d56051227a6b38509cfe5ee930a
leejung7308 <leekkzzzz@naver.com>
2024-02-23 02:38:27 +0900

    feat: Add Game Over

--- Assets/Scripts/Interaction/Interact_heal.cs
+++ Assets/Scripts/Interaction/Interact_heal.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
-using UnityEditor.Build.Player;
 using UnityEngine;
 public class Interact_heal : MonoBehaviour
 {
